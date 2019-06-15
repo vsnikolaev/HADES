ee0f4eda0e2bf06f94013c1c2ee7adaa91062424
nvs <nikolaev.vasia@gmail.com>
2019-06-16 01:10:59 +0300

    sh del extra letter

--- batch.sh
+++ batch.sh
@@ -17,4 +17,4 @@ job_range=1-$n_runs
 
 echo executing job range $job_range
 
-sbatch -J resolution -p $partition -t $time -a $job_range -e ${log_dir}/%A_%a.e -o ${log_dir}/%A_%a.o --exporte=file_list=$file_list,hadesroot=$hadesroot batch_run.sh
+sbatch -J resolution -p $partition -t $time -a $job_range -e ${log_dir}/%A_%a.e -o ${log_dir}/%A_%a.o --export=file_list=$file_list,hadesroot=$hadesroot batch_run.sh
