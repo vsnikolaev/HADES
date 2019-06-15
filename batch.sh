b89c8ae8978355945e0efcbb76a03c2fb3bfb12d
nvs <nikolaev.vasia@gmail.com>
2019-06-15 21:18:05 +0300

    sh last

--- batch.sh
+++ batch.sh
@@ -6,12 +6,10 @@ current_dir=$(pwd)
 partition=main
 time=8:00:00
 
+mkdir -p r/log
 log_dir=r/log
 
 file_list=${1}
-
-
-mkdir -p $r
 
 n_runs=$(cat $file_list | wc -l)
 
@@ -19,4 +17,4 @@ job_range=1-$n_runs
 
 echo executing job range $job_range
 
-sbatch -J resolution -p $partition -t $time -a $job_range -e ${log_dir}/%A_%a.e -o ${log_dir}/%A_%a.o --export=executable=$executable,output_dir=$output_dir,file_list=$file_list,hadesroot=$hadesroot,cmd=$cmd,signal=$signal,channelSelection=$channelSelection,minSignal=$minSignal,maxSignal=$maxSignal,pidCode=$pidCode batch_run.sh
+sbatch -J resolution -p $partition -t $time -a $job_range -e ${log_dir}/%A_%a.e -o ${log_dir}/%A_%a.o --exporte=file_list=$file_list,hadesroot=$hadesroot batch_run.sh
--- batch_run.sh
+++ batch_run.sh
@@ -11,8 +11,8 @@ input_file=`sed -n "${job_num}p" < $file_list`
 echo "loading " $hadesroot
 source $hadesroot
 
-echo "executing $root -l rootlogon.c 'reader_resolution.cpp(file)' $cmd --signal
+echo "executing $root -l rootlogon.c "reader_resolution.cpp($file)"
 
-$root -l rootlogon.c 'reader_resolution.cpp(file)' 
+$root -l "rootlogon.c" "reader_resolution.cpp($file)" 
 echo JOB FINISHED!
 date $format
