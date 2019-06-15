f0070e111fac1c3b8c61ce8817b7d2d16ddc7952
nvs <nikolaev.vasia@gmail.com>
2019-06-15 21:33:39 +0300

    sh

--- batch_run.sh
+++ batch_run.sh
@@ -5,13 +5,13 @@ format='+%Y/%m/%d-%H:%M:%S'
 date $format
 
 job_num=$(($SLURM_ARRAY_TASK_ID))
-output_file=$output_dir/$job_num
 input_file=`sed -n "${job_num}p" < $file_list`
+file="$input_file"
 
 echo "loading " $hadesroot
 source $hadesroot
 
-echo "executing $root -l rootlogon.c "reader_resolution.cpp($file)"
+echo executing $root -l "rootlogon.c" "reader_resolution.cpp($file)"
 
 $root -l "rootlogon.c" "reader_resolution.cpp($file)" 
 echo JOB FINISHED!
